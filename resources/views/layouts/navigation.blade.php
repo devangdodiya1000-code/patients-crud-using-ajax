5c1917441d77ff70be8c08544b59ce21f7d44629
Devang Dodiya <devangdodiya1000@gmail.com>
2026-05-03 16:24:43 +0530

    change the title name dynamic

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,7 +28,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('patients')" :active="request()->routeIs('patients')">
-                        {{ __('Patient') }}
+                        {{ __('Patients') }}
                     </x-nav-link>
                 </div>
             </div>
